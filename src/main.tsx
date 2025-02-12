415b5389222407752544dfa9335fa4475f57f060
Tomm Jobit <tommjobit@live.fr>
2025-02-12 17:35:52 +0100

    auth pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Homepage from './pages/Homepage'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Homepage from "./pages/Homepage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
+import React from "react";
+import Authpage from "./pages/Authpage";
+import Cart from "./pages/Cart";
+
+function isAuthenticated() {
+  const token = localStorage.getItem("accessToken");
+
+  return token ? true : false;
+}
+
+function ProtectedRoute({ element }: { element: React.ReactNode }) {
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!isAuthenticated()) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return <>{element}</>;
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
   },
-])
+  {
+    path: "/auth",
+    element: <Authpage />,
+  },
+  {
+    path: "/cart",
+    element: <ProtectedRoute element={<Cart />} />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
